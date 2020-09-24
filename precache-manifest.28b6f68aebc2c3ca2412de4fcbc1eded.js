e3a86787a5873ce21e43f8794e2d530d891777b0
James Pereira <james.pereira90@gmail.com>
2020-09-24 16:32:20 +1000

    Updates

---
+++ precache-manifest.28b6f68aebc2c3ca2412de4fcbc1eded.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d174303e0b54a1d82cbb13d093d6473d",
+    "url": "/my-games/index.html"
+  },
+  {
+    "revision": "2c1ff0e889219be51e1c",
+    "url": "/my-games/static/css/main.4b29d48d.chunk.css"
+  },
+  {
+    "revision": "eb8b6c19b60cff80132f",
+    "url": "/my-games/static/js/2.b3ed339f.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/my-games/static/js/2.b3ed339f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c1ff0e889219be51e1c",
+    "url": "/my-games/static/js/main.7366468b.chunk.js"
+  },
+  {
+    "revision": "c50f3608805ce94b3b61",
+    "url": "/my-games/static/js/runtime-main.80907682.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/my-games/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/my-games/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/my-games/static/media/arrow-drop-down.78c6f53b.svg"
+  }
+]);
